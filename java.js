75d6d4e2c154dc65d7ea475e783ca73e69a6bd12
Your Name <your_email@example.com>
2024-08-24 23:12:06 -1100

    this commit

--- java.js
+++ java.js
@@ -1,5 +1,13 @@
+// var number=[11,22,33,44,55,66,77,88,99];
+// var result=number.filter(function(item){
+//     return item>30;
+// })
+// console.log(result);
+
+// This is find array
+
 var number=[11,22,33,44,55,66,77,88,99];
-var result=number.filter(function(item){
-    return item>30;
+var result=number.find(function(value){
+    return value>50;
 })
-console.log(result);
+console.log(result)
